2e3b9b996ee351c586bcb16e6a5c69485f3b62db
José Ignacio Litninsky <Jose.Litninsky@alu.frt.utn.edu.ar>
2026-09-08 21:14:39 -0300

    Agregar Clase Producto.cs con propiedades basicas

---
+++ Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorralonQuoter
+{
+    internal class Producto
+    {
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public string Proveedor { get; set; }
+        public string Marca { get; set; }
+        public string Rubro { get; set; }
+        public decimal Precio { get; set; }
+        public decimal Stock { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            var p = new Producto();
+            p.Precio = 9850.50m;
+            p.Descripcion = "Cemento Portland x 50kg";
+            Console.WriteLine(p.Descripcion + " - $" + p.Precio);
         }
     }
 }
